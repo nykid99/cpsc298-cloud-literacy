99765f2e1f1a94cd0d74da48ba763468f5a9743b
Christopher <cyoun@chapman.edu>
2021-03-07 10:50:08 -0800

    fix permission issues

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 sudo yum update -y
 sudo yum groupinstall 'Development Tools' -y
 sudo yum install httpd -y
-mkdir /youn_code
+sudo mkdir /youn_code
 cd /youn_code
-git clone --branch christopher https://github.com/christofuy/cpsc298-cloud-literacy.git
+sudo git clone --branch christopher https://github.com/christofuy/cpsc298-cloud-literacy.git
 sudo service httpd start
 sudo chkconfig httpd on
 cd cpsc298-cloud-literacy/renderer/raytrace
