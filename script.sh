b691fa2be5f54f61006d8f253cd874dd62e03fa6
BJ Bae <41602923+01bbae@users.noreply.github.com>
2021-03-10 20:42:48 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,11 +4,11 @@ yum groupinstall 'Development Tools' -y
 yum install httpd -y
 mkdir /wcode
 cd /wcode
-git clone --branch 298-bbae https://github.com/harteratchapman/cpsc298-cloud-literacy.git
+git clone --branch bbae-298 https://github.com/harteratchapman/cpsc298-cloud-literacy.git
 service httpd start
 chkconfig httpd on
 cd /wcode/cpsc298-cloud-literacy/renderer/raytrace
 ./build
 ./raytrace
 cp torus.png /var/www/html/myshape.png
-cp /wcode/cpsc298-cloud-literacy/298-bbae/index.html /var/www/html
+cp /wcode/cpsc298-cloud-literacy/bbae-298/index.html /var/www/html
